bf130176119ae61bcb7e976e48b80af4ae47217d
banderGlitch <nernaykumar98@gmail.com>
2025-01-29 13:36:23 +0530

    changes

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import "../styles/Footer.css";
+
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+        <div className="footer-container">
+            <p className="footer-text">Copyright &copy; 2025 Blog Post</p>
+        </div>
+    </footer>
+
+
+
+
+
   )
 }
