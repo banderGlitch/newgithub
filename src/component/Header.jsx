7a75f74e453950ea150311a698939ba9eb10ea68
banderGlitch <nernaykumar98@gmail.com>
2025-01-29 13:29:40 +0530

    changes

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import "../styles/Header.css";
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
 
 export default function Header() {
   return (
     <header className="header">
         <div className="container">
-            <h1 className="logo">My website</h1>
+            <h1 className="logo">Blog Post</h1>
             <nav>
                 <ul className="nav-links">
-                    <Link to="/">Home</Link>
-                    <Link to="/about">About</Link>
-                    <Link to="/contact">Contact</Link>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/about">About</NavLink>
+                    <NavLink to="/contact">Contact</NavLink>
                 </ul>
-
             </nav>
-
         </div>
     </header>
 
